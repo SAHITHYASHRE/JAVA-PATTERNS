487a3779f05402189cddbaf58faa60af97fef41c
SAHITHYASHRE <113914614+SAHITHYASHRE@users.noreply.github.com>
2024-07-21 19:49:29 +0530

    Create Rhombus pattern.java

---
+++ Rhombus pattern.java
@@ -0,0 +1,23 @@
+class HelloWorld {
+    public static void main(String[] args) {
+       	 int n=5;
+        for(int i=0;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+      
+}
+
+o/p:
+      *****
+     *****
+    *****
+   *****
+  *****
+ *****
